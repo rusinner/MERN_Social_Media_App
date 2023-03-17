a81de7da3d80c5c534cbd5a2fe0956af564f25c3
Theologos Mavropoulos <rusinner88@gmail.com>
2023-03-17 13:41:05 +0200

    set authSlice in redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
-import "./App.css";
+import { useState } from "react";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./scenes/homePage";
+import LoginPage from "./scenes/loginPage";
+import ProfilePage from "./scenes/profilePage";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
