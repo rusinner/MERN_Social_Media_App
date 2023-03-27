d942b4091239adc010c3fdf85a6a9f24163668e6
Theologos Mavropoulos <rusinner88@gmail.com>
2023-03-27 19:53:19 +0300

    Profile Page

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -2,7 +2,7 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import Navbar from "../navbar";
+import Navbar from "../navbar/index";
 import FriendListWidget from "../widgets/FriendsListWidget";
 import MyPostWidget from "../widgets/MyPostWidget";
 import PostsWidget from "../widgets/PostsWidget";
@@ -28,33 +28,34 @@ const ProfilePage = () => {
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   if (!user) return null;
-
-  return (
-    <Box>
-      <Navbar />
-      <Box
-        width="100%"
-        padding="2rem 6%"
-        display={isNonMobileScreens ? "flex" : "block"}
-        gap="2rem"
-        justifyContent="center"
-      >
-        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
-          <UserWidget userId={userId} picturePath={user.picturePath} />
-          <Box m="2rem 0" />
-          <FriendListWidget userId={userId} />
-        </Box>
+  else {
+    return (
+      <Box>
+        <Navbar />
         <Box
-          flexBasis={isNonMobileScreens ? "42%" : undefined}
-          mt={isNonMobileScreens ? undefined : "2rem"}
+          width="100%"
+          padding="2rem 6%"
+          display={isNonMobileScreens ? "flex" : "block"}
+          gap="2rem"
+          justifyContent="center"
         >
-          <MyPostWidget picturePath={user.picturePath} />
-          <Box m="2rem 0" />
-          <PostsWidget userId={userId} isProfile />
+          <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+            <UserWidget userId={userId} picturePath={user.picturePath} />
+            <Box m="2rem 0" />
+            <FriendListWidget userId={userId} />
+          </Box>
+          <Box
+            flexBasis={isNonMobileScreens ? "42%" : undefined}
+            mt={isNonMobileScreens ? undefined : "2rem"}
+          >
+            <MyPostWidget picturePath={user.picturePath} />
+            <Box m="2rem 0" />
+            <PostsWidget userId={userId} isProfile />
+          </Box>
         </Box>
       </Box>
-    </Box>
-  );
+    );
+  }
 };
 
 export default ProfilePage;
